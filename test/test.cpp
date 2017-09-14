dbf7516780bd67d14b22727fcfe475463e39306a
Jeff Hill <jeffrey.p.hill@nasa.gov>
2017-09-14 16:07:49 -0700

    Add Eigen library; create test to verify include paths, etc.

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 #include "catch.hpp"
+
 
 TEST_CASE("Verify Catch is up and running") {
     REQUIRE(1 == 1);
+}
+
+TEST_CASE("Verify Eigen is up and running") {
+    using Eigen::Vector3d;
+    Vector3d x( 1.0, 3.0, 0.0 );
+    Vector3d y( 4.0, 2.0, 0.0 );
+    Vector3d z = x.cross(y);
+    REQUIRE( z[0] ==   0.0 );
+    REQUIRE( z[1] ==   0.0 );
+    REQUIRE( z[2] == -10.0 );
 }
 
