fa102463c1fac3a693a463e4e63162a8785880ac
Jeff Hill <jeff.p.hill@gmail.com>
2017-10-14 17:38:37 -0700

    Make test pass in both debug and release.

--- jflow_test/test_jflow.cpp
+++ jflow_test/test_jflow.cpp
@@ -1,9 +1,15 @@
 #include "catch.hpp"
 #include "jflow.hpp"
 
+#ifdef NDEBUG
+  #define REQUIRE_THROWS_IF_DEBUG  REQUIRE_NOTHROW
+#else
+  #define REQUIRE_THROWS_IF_DEBUG  REQUIRE_THROWS
+#endif
+
 TEST_CASE("test check_precondition function") {
     jflow::check_precondition(true, "this should not throw");
-    REQUIRE_THROWS(jflow::check_precondition(false, "this should throw"));
+    REQUIRE_THROWS_IF_DEBUG(jflow::check_precondition(false, "this should throw"));
 }
 
 TEST_CASE("test cross product function (vector2 overload)") {
--- jflow_test/test_structured_grid.cpp
+++ jflow_test/test_structured_grid.cpp
@@ -5,6 +5,12 @@
 #include <filesystem>
 #include <iostream>
 
+#ifdef NDEBUG
+  #define REQUIRE_THROWS_IF_DEBUG  REQUIRE_NOTHROW
+#else
+  #define REQUIRE_THROWS_IF_DEBUG  REQUIRE_THROWS
+#endif
+
 TEST_CASE("test structured_grid class") {
     // Creates a simple 5-by-3 point test gird and verifies that our
     // grid methods work as intended. Grid looks like this:
@@ -32,10 +38,10 @@ TEST_CASE("test structured_grid class") {
         REQUIRE((grid.vertex(2, 1) == vec2{ 0.0, 0.0 }));
         REQUIRE((grid.vertex(0, 2) == vec2{ -2.0, 1.0 }));
         REQUIRE((grid.vertex(4, 0) == vec2{ 2.0, -1.0 }));
-        REQUIRE_THROWS(grid.vertex(0, -1));
-        REQUIRE_THROWS(grid.vertex(0, 3));
-        REQUIRE_THROWS(grid.vertex(-1, 0));
-        REQUIRE_THROWS(grid.vertex(5, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.vertex(0, -1));
+        REQUIRE_THROWS_IF_DEBUG(grid.vertex(0, 3));
+        REQUIRE_THROWS_IF_DEBUG(grid.vertex(-1, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.vertex(5, 0));
     }
     SECTION("Test iface indexing via (i,j) coordinates)") {
         // This *looks* backwards but is correct. We want the edge tangent
@@ -46,20 +52,20 @@ TEST_CASE("test structured_grid class") {
         REQUIRE((grid.iface(0, 0).vertex(1) == grid.vertex(0, 0)));
         REQUIRE((grid.iface(4, 1).vertex(0) == grid.vertex(4, 2)));
         REQUIRE((grid.iface(4, 1).vertex(1) == grid.vertex(4, 1)));
-        REQUIRE_THROWS(grid.iface(-1, 0));
-        REQUIRE_THROWS(grid.iface(5, 0));
-        REQUIRE_THROWS(grid.iface(0, -1));
-        REQUIRE_THROWS(grid.iface(0, 2));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(-1, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(5, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(0, -1));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(0, 2));
     }
     SECTION("Test jface indexing via (i,j) coordinates)") {
         REQUIRE((grid.jface(0, 0).vertex(0) == grid.vertex(0, 0)));
         REQUIRE((grid.jface(0, 0).vertex(1) == grid.vertex(1, 0)));
         REQUIRE((grid.jface(3, 2).vertex(0) == grid.vertex(3, 2)));
         REQUIRE((grid.jface(3, 2).vertex(1) == grid.vertex(4, 2)));
-        REQUIRE_THROWS(grid.jface(-1, 0));
-        REQUIRE_THROWS(grid.jface(4, 0));
-        REQUIRE_THROWS(grid.jface(0, -1));
-        REQUIRE_THROWS(grid.jface(0, 3));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(-1, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(4, 0));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(0, -1));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(0, 3));
     }
     SECTION("Test iface/jface area() accessor") {
         REQUIRE((grid.iface(0, 0).area() == vec2{ 1.0, 0.0 }));
@@ -70,33 +76,33 @@ TEST_CASE("test structured_grid class") {
     SECTION("Test iface/jface cell() accessor") {
         REQUIRE((grid.iface(1, 1).cell(0) == grid.cell(0, 1)));
         REQUIRE((grid.iface(1, 1).cell(1) == grid.cell(1, 1)));
-        REQUIRE_THROWS(grid.iface(0, 0).cell(0));
-        REQUIRE_THROWS(grid.iface(4, 0).cell(1));
-        REQUIRE_THROWS(grid.iface(1, 1).cell(2));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(0, 0).cell(0));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(4, 0).cell(1));
+        REQUIRE_THROWS_IF_DEBUG(grid.iface(1, 1).cell(2));
 
         REQUIRE((grid.jface(1, 1).cell(0) == grid.cell(1, 0)));
         REQUIRE((grid.jface(1, 1).cell(1) == grid.cell(1, 1)));
-        REQUIRE_THROWS(grid.jface(0, 0).cell(0));
-        REQUIRE_THROWS(grid.jface(2, 2).cell(1));
-        REQUIRE_THROWS(grid.jface(1, 1).cell(2));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(0, 0).cell(0));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(2, 2).cell(1));
+        REQUIRE_THROWS_IF_DEBUG(grid.jface(1, 1).cell(2));
     }
     SECTION("Test cell indexing via (i,j) coordinates)") {
         REQUIRE((grid.cell(0, 0).vertex(0) == grid.vertex(0, 0)));
         REQUIRE((grid.cell(2, 1).vertex(2) == grid.vertex(3, 2)));
-        REQUIRE_THROWS((grid.cell(-1, 0)));
-        REQUIRE_THROWS((grid.cell(4, 0)));
-        REQUIRE_THROWS((grid.cell(0, -1)));
-        REQUIRE_THROWS((grid.cell(0, 2)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(-1, 0)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(4, 0)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, -1)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, 2)));
     }
     SECTION("Test cell face indexing") {
         REQUIRE((grid.cell(1, 0).iface(0) == grid.iface(1, 0)));
         REQUIRE((grid.cell(1, 0).iface(1) == grid.iface(2, 0)));
         REQUIRE((grid.cell(2, 1).jface(0) == grid.jface(2, 1)));
         REQUIRE((grid.cell(2, 1).jface(1) == grid.jface(2, 2)));
-        REQUIRE_THROWS((grid.cell(0, 0).iface(-1)));
-        REQUIRE_THROWS((grid.cell(0, 0).iface(2)));
-        REQUIRE_THROWS((grid.cell(0, 0).jface(-1)));
-        REQUIRE_THROWS((grid.cell(0, 0).jface(2)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, 0).iface(-1)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, 0).iface(2)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, 0).jface(-1)));
+        REQUIRE_THROWS_IF_DEBUG((grid.cell(0, 0).jface(2)));
     }
     SECTION("Test cell volume calculation") {
         REQUIRE((grid.cell(0, 0).volume() == Approx(1.0)));
