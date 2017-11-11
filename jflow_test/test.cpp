452af3b7437f8f1c598b3962312dcb3a45f577a6
Jeff Hill <jeff.p.hill@gmail.com>
2017-11-11 14:03:43 -0800

    Add blaze as external library.

--- jflow_test/test.cpp
+++ jflow_test/test.cpp
@@ -1,6 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include <Eigen/Core>
+#include <blaze/Math.h>
+
+TEST_CASE("Verfiy Blaze is up and running") {
+    using blaze::DynamicVector;
+    using blaze::StaticVector;
+    StaticVector<int, 3UL> a{ 4, -2, 5 };
+    DynamicVector<int> b{ 2, 5, -3 };
+    auto c = a + b;
+    REQUIRE(c[0] == 6);
+    REQUIRE(c[1] == 3);
+    REQUIRE(c[2] == 2);
+}
 
 TEST_CASE("Verify Catch is up and running") {
     REQUIRE(1 == 1);
